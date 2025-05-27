1124abe3e729c2340f519633468c698385fa5854
PeeyushDas <122cs0071@nitrkl.ac.in>
2025-05-27 08:45:18 +0530

    build: config files

---
+++ lib/config/app_constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppConstants {
+  // App Information
+  static const String appName = 'Disease Classifier';
+  static const String appVersion = '1.0.0';
+
+  // colors
+  static const Color primaryColor = Color(0xFF4CAF50);
+  static const Color secondaryColor = Color(0xFF2E7D32);
+  static const Color neutral1 = Color(0xFF616161);
+
+  // API Endpoints
+  static const String serverUrl =
+      "https://rice-server-j666.onrender.com/predict/";
+}
